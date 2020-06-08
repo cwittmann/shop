447ebe7ea0f37f1fc2386f6c3bf2498e1cbd8371
cwittmann <christofwittmann@web.de>
2020-06-08 16:51:34 +0200

    feat: username & password in db

--- src/main/java/com/shop/controller/AuthController.java
+++ src/main/java/com/shop/controller/AuthController.java
@@ -1,5 +1,11 @@
 package com.shop.controller;
 
+import java.util.Base64;
+
+import com.shop.model.User;
+import com.shop.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,14 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/auth")
     public ResponseEntity<String> authenticate(@RequestHeader("authorization") String authorization) {
-        if (authorization.contains("dXNlcjE6cGFzc3dvcmQx")) {
+
+        String base64Credentials = authorization.substring("Basic".length()).trim();
+        String decoded = new String(Base64.getDecoder().decode(base64Credentials));
+        String user = decoded.split(":")[0];
+        String password = decoded.split(":")[1];
+
+        User userFromDb = userRepository.findByUserName(user);
+
+        if (userFromDb != null && userFromDb.password.equals(password)) {
             return new ResponseEntity<String>("Successful", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<String>("Error", HttpStatus.NOT_FOUND);
         }
 
+        return new ResponseEntity<String>("Error", HttpStatus.NOT_FOUND);
     }
-
 }
--- src/main/java/com/shop/model/User.java
+++ src/main/java/com/shop/model/User.java
@@ -8,14 +8,19 @@ public class User {
     public String firstName;
     public String lastName;
     public String city;
+    public String userName;
+    public String password;
     public String roleId;
     public Role role;
 
-    public User(String id, String firstName, String lastName, String city, String roleId, Role role) {
+    public User(String id, String firstName, String lastName, String city, String userName, String password,
+            String roleId, Role role) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.city = city;
+        this.userName = userName;
+        this.password = password;
         this.roleId = roleId;
         this.role = role;
     }
