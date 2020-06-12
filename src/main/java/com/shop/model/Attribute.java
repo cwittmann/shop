4d305db3729f6c7469bfb68c8950a85d3d5daf02
cwittmann <christofwittmann@web.de>
2020-06-12 16:58:22 +0200

    feat: productId in attribute

--- src/main/java/com/shop/model/Attribute.java
+++ src/main/java/com/shop/model/Attribute.java
@@ -5,11 +5,13 @@ import org.springframework.data.annotation.Id;
 public class Attribute {
     @Id
     public String id;
+    public String productId;
     public String name;
     public String value;
 
-    public Attribute(String id, String name, String value) {
+    public Attribute(String id, String productId, String name, String value) {
         this.id = id;
+        this.productId = productId;
         this.name = name;
         this.value = value;
     }
