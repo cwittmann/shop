4efc0c181c948b16d6b2ae386305c201b480e891
cwittmann <christofwittmann@web.de>
2020-06-19 15:18:25 +0200

    fix: removed unused imports

--- src/main/java/com/shop/controller/AuthController.java
+++ src/main/java/com/shop/controller/AuthController.java
@@ -1,18 +1,17 @@
 package com.shop.controller;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 import com.shop.model.User;
 import com.shop.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
