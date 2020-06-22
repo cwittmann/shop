53e907e208bb3fec34bca85041c87314da6ffb0e
cwittmann <christofwittmann@web.de>
2020-06-22 16:31:04 +0200

    feat: attribute with info

--- src/main/java/com/shop/model/Attribute.java
+++ src/main/java/com/shop/model/Attribute.java
@@ -8,11 +8,13 @@ public class Attribute {
     public String productId;
     public String name;
     public String value;
+    public String info;
 
-    public Attribute(String id, String productId, String name, String value) {
+    public Attribute(String id, String productId, String name, String value, String info) {
         this.id = id;
         this.productId = productId;
         this.name = name;
         this.value = value;
+        this.info = info;
     }
 }
